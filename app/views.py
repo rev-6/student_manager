a2f6bdae199552d4005c7e2c07b9c6f07c52831c
Костя <kostya223home@gmail.com>
2025-12-03 21:15:55 +0300

    add main

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django import forms
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+import datetime
 
-# Create your views here.
+def main(request):
+    return render(request, 'main.html')
